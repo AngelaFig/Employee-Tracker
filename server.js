1a9fb6ba14a45c5149ac551c65d0be2ac0de9e76
AngelaFig <angelafigueroa596@gmail.com>
2023-08-22 12:39:21 -0400

    added a few prompt questions to begin testing functionaity..not working as of now

--- server.js
+++ server.js
@@ -16,13 +16,38 @@ db.promise().query('SELECT * FROM positions_db')
 .then((rows)=> console.log(rows))
 .catch((err)=>console.log(err));
 
-db.promise().query('SELECT department_name FROM department')
-.then((name)=>{
-    let departmentArray = []
-    name[0].forEach(element =>{
-        departmentArray.push(element.department_name)
-    });
-    console.log(departmentArray)
-}) 
-.catch((err)=>console.log(err));
+
+
+// db.promise().query('SELECT department_name FROM department')
+// .then((name)=>{
+//     let departmentArray = []
+//     name[0].forEach(element =>{
+//         departmentArray.push(element.department_name)
+//     });
+//     console.log(departmentArray)
+// }) 
+// .catch((err)=>console.log(err));
+
+
+const inputs = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ["View all Departments", 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role',
+        'Add an Employee', 'Add and Employee Role', 'Quit'],
+        name: "startOptions"
+    },
+    {
+        type:'input',
+        message: 'Enter the name of the Department',
+        name: 'inputDepartmentName',
+        when: (response)=>(response.startOptions === 'Add a Department')
+    },
+    {
+        type:'input',
+        message: 'Enter the name of the Role',
+        name: 'inputRoleName',
+        when: (response)=>(response.startOptions === 'Add a Role')
+    },
+]
 
